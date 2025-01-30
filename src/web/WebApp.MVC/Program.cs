a08fb86728ac0de77bf8a542b2bb15d877d551ef
Ernane Sa <ernane.sa@gmail.com>
2025-01-30 10:46:13 -0300

    Adiciona configuração de HttpClient no serviço de autenticação

--- src/web/WebApp.MVC/Program.cs
+++ src/web/WebApp.MVC/Program.cs
@@ -1,29 +1,20 @@
+using WebApp.MVC.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddIdentityConfiguration();
+builder.Services.AddWebAppConfiguration(builder.Configuration);
+builder.Services.RegisterServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.UseWebAppConfiguration(app.Environment);
 
 app.Run();
